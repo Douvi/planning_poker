3fbdd54f4368b5b72fcfb8ad8bcec4ecbe590643
Edouard Roussillon <edouard.roussillon@gmail.com>
2020-11-03 23:46:56 -0300

    update db column default value

--- priv/repo/migrations/20201028022518_update_tables copy.exs
+++ priv/repo/migrations/20201028022518_update_tables copy.exs
@@ -3,7 +3,7 @@ defmodule PandaPhoenix.Repo.Migrations.UpdateFieldPrivateMessage do
 
   def up do
     execute """
-      ALTER TABLE ONLY tables ALTER COLUMN voting_rule SET DEFAULT '0, 1, 2, 3, 5, 8, 13, 21';
+      ALTER TABLE ONLY tables ALTER COLUMN countdown SET DEFAULT 30;
     """
   end
 end
